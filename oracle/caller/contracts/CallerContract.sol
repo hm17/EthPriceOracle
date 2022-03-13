64d3ce6783eb852ca887f96ff125d06bcb336082
hazel <hazel.madolid@gmail.com>
2022-03-13 13:26:17 -0500

    created caller contract

---
+++ oracle/caller/contracts/CallerContract.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.5.0;
+
+import "./EthPriceOracleInterface.sol";
+
+contract CallerContract {
+    EthPriceOracleInterface private oracleInstance;
+    address private oracleAddress;
+
+    function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+        oracleAddress = _oracleInstanceAddress;
+        oracleInstance = EthPriceOracleInterface(oracleAddress);
+    }
+}
